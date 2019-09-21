486061a6897e34d3c0a8b0f6db73460a2e69373e
Todd Traylor <toddt67878@yahoo.com>
2019-09-21 13:43:11 -0500

    Correctly postioned //= require jquery-ui line of code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,7 @@ require("channels")
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require_tree .
-//= require jquery-ui
+
